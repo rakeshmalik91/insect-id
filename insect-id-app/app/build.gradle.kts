697a388c185f442d2b1536358e7c674cc76b4217
Rakesh Malik <rakeshmalik91@gmail.com>
2025-07-21 07:37:39 +0530

    app release v0.0.3 alpha

--- insect-id-app/app/build.gradle.kts
+++ insect-id-app/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.rakeshmalik.insectid"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
